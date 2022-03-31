3cfa8a6cd7903a5d14ce2a46de5cc337e2852b38
Matthew Garafalo <matt9194@gmail.com>
2022-03-31 18:50:49 -0400

    New Foundation
    Had to remove previous dotnet build as installed version of CRA was causing dependency issues with multiple packages that are apart of the component library Chakra UI that we are going to be using (for now)

---
+++ clientapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      
+    </ChakraProvider>
+  );
+}
+
+export default App;
